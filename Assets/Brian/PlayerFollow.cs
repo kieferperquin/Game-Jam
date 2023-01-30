fd3b99a0caeaa5e6521e2adfe37ea8c09a6c3535
C-h-a-o-z <b.somers@sintlucasedu.nl>
2023-01-30 14:43:28 +0100

    camera follow

---
+++ Assets/Brian/PlayerFollow.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFollow : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    [SerializeField] bool startFollowing;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (startFollowing)
+        {
+            transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+        }
+    }
+}
