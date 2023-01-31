184cd86d39a72ab89c108553219449aae102697f
C-h-a-o-z <b.somers@sintlucasedu.nl>
2023-01-31 11:47:44 +0100

    follow scene

--- Assets/Brian/PlayerFollow.cs
+++ Assets/Brian/PlayerFollow.cs
@@ -6,11 +6,20 @@ public class PlayerFollow : MonoBehaviour
 {
     [SerializeField] GameObject player;
     [SerializeField] bool startFollowing;
+    [SerializeField] bool FollowX = true;
+    [SerializeField] bool FollowY = false;
     void FixedUpdate()
     {
         if (startFollowing)
         {
+            if (FollowX)
+            {
             transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+            }
+            if (FollowY)
+            {
+                transform.position = new Vector3(transform.position.x, player.transform.position.y, transform.position.z);
+            }
         }
     }
     public void Follow()
@@ -20,5 +29,10 @@ public class PlayerFollow : MonoBehaviour
     public void StopFollow()
     {
         startFollowing = true;
+    }
+    public void SpecificFollow(bool folX, bool folY)
+    {
+        FollowX = folX;
+        FollowY = folY;
     }
 }
