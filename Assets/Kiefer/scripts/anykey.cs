48db81e25e76a1b54d2dd63834a28d335c5d0057
kieferperquin <91886036+kieferperquin@users.noreply.github.com>
2023-01-31 14:04:50 +0100

    .

---
+++ Assets/Kiefer/scripts/anykey.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class anykey : MonoBehaviour
+{
+    
+    void Update()
+    {
+        if (Input.anyKey)
+        {
+
+        }
+    }
+}
